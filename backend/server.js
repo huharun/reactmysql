e3feb9537152aeb9b50b946fad96ca1cad7473b8
arun <arunramkrishna997@gmail.com>
2024-09-21 11:40:12 -0400

    new

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,29 @@
-const express = require('express'); const mysql = require('mysql'); const cors = require('cors');
+const express = require('express'); 
+const mysql = require('mysql'); 
+const cors = require('cors');
 
-const app = express(); const port = 8081;
+const app = express(); 
+const port = 8081;
 
 // Middleware to enable CORS
 app.use(cors());
 
 // Database connection
-const db = mysql.createConnection({ host: 'localhost', user: 'root', password: '', database: 'hospital' });
+const db = mysql.createConnection({ 
+  host: 'localhost', 
+  user: 'root', 
+  password: '', 
+  database: 'hospital' 
+});
 
 // Connect to the database
-db.connect(err => { if (err) { console.error('Database connection failed:', err); return; } console.log('Database connected!'); });
+db.connect(err => { 
+  if (err) { 
+    console.error('Database connection failed:', err); 
+    return; 
+  } 
+    console.log('Database connected!'); 
+  });
 
 // // when the browser points to localhost:8081/listall
 // app.get('/listall', (request, response) => {
@@ -26,8 +40,22 @@ const tableNames = [
     'medications', 'surgeries', 'bills', 'nurses', 'wards'
   ];
   
+  
 // Create endpoints for all tables
-tableNames.forEach(table => { app.get(`/${table}`, (req, res) => { const sql = `SELECT * FROM ${table}`; db.query(sql, (err, data) => { if (err) { console.error(`Error executing query for ${table}:`, err); return res.status(500).json({ error: 'Database query failed' }); } return res.json(data); }); }); });
+const data =  tableNames.forEach(table => { 
+  app.get(`/${table}`, (req, res) => { 
+    const sql = `SELECT * FROM ${table}`; 
+    db.query(sql, (err, data) => { 
+      if (err) { console.error(`Error executing query for ${table}:`, err); 
+      return res.status(500).json({ error: 'Database query failed' }); 
+      } 
+      return res.json(data); 
+    }); 
+  }); 
+});
+
 
 // Start the server
-app.listen(port, () => { console.log(`Server running at http://localhost:${port}`); });
+app.listen(port, () => { 
+  console.log(`Server running at http://localhost:${port}`); 
+});
